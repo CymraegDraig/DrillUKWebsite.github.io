84a1e23f951180fd4cb2af1ea7e5c9fca3ff53d4
CymraegDraig <95748656+DraqnnDevelopment@users.noreply.github.com>
2024-04-01 00:11:43 +0100

    Added Afterdark Support

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const config = {
         serverLogoImageFileName: "logo.png", /*This is a file name for logo in /images/ (If you upload new logo with other name, you must change this value)*/
         serverName: "Drill-UK", /*Server name*/
         serverIp: "fivem.drilluk.uk", /*Server IP (if you want to add online user counter, you must have true the enable-status and enable-query of server.properties)*/
+        serverIp2: "DrillUK | Afterdark", /*Server IP (if you want to add online user counter, you must have true the enable-status and enable-query of server.properties)*/
         discordServerID: "1033780064996827227" /*Your server ID (if you want to add online user counter, you must have enabled Discord server widget)*/
     },
 
@@ -251,7 +252,7 @@ const config = {
             {
                 inGameName: "Kev",
                 rank: "Trial Moderator",
-                skinUrlOrPathToFile: "images/Staff/Kev.png",
+                skinUrlOrPathToFile: "images/Staff/Kev2.png",
                 rankColor: ""
             },
             {
