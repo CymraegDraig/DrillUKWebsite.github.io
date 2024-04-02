5606f8c604d638483bd9293e1168f73a0d0e4144
Tobs <93937426+t4by@users.noreply.github.com>
2024-04-02 13:04:43 +0100

    lolololol

--- js/fivem-semi.js
+++ js/fivem-semi.js
@@ -401,6 +401,7 @@ const setDataFromConfigToHtml = async () => {
 
     let locationPathname = location.pathname;
 
+    if(locationPathname == "/" || locationPathname.includes("fivem"))
     if(locationPathname == "/" || locationPathname.includes("index")) {
         copyIp();
         /*Set config data to header*/
