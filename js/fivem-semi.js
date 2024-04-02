9aee1e407ff69079a695d2c3dee3df145a874ac7
Tobs <93937426+t4by@users.noreply.github.com>
2024-04-02 12:30:11 +0100

    to all my killas and 100 dollar

--- js/fivem-semi.js
+++ js/fivem-semi.js
@@ -401,7 +401,7 @@ const setDataFromConfigToHtml = async () => {
 
     let locationPathname = location.pathname;
 
-    if(locationPathname == "/" || locationPathname.includes("index")) {
+    if(locationPathname === "/" || locationPathname.endsWith(".html")) {
         copyIp();
         /*Set config data to header*/
         serverLogoHeader.src = `images/` + config.serverInfo.serverLogoImageFileName;
